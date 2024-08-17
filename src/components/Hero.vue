5268843e5d9cbb4d84a480e3f91a6b15ba2237ad
Alberto Vigna <betoveluis@gmail.com>
2024-08-17 14:44:24 -0500

    Changes in the color palette and logo.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,17 +4,13 @@
 
 <template>
   <!-- Hero -->
-  <section class="bg-green-700 py-20 mb-4">
-    <div
-        class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-    >
+  <section class="bg-[#F2E74B] py-20 mb-4">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <div class="text-center">
-        <h1
-            class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-        >
+        <h1 class="text-4xl font-extrabold text-[#010D00] sm:text-5xl md:text-6xl">
           SPOC
         </h1>
-        <p class="my-4 text-xl text-white">
+        <p class="my-4 text-xl text-[#735620]">
           Signal Processing on Cloud
         </p>
       </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <script setup>
-import logo from '../assets/img/logo.png';
+import logo from '../assets/img/logo.svg';
 </script>
 
 <template>
-  <nav class="bg-green-700 border-b border-green-500">
+  <nav class="bg-[#010D00] border-b border-[#735620]">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div
@@ -12,7 +12,7 @@ import logo from '../assets/img/logo.png';
           <!-- Logo -->
           <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
             <img class="h-10 w-auto" v-bind:src="logo" alt="SPOC - Signal Processing on Cloud" />
-            <span class="hidden md:block text-white text-2xl font-bold ml-2"
+            <span class="hidden md:block text-[#F2E74B] text-2xl font-bold ml-2"
             >SPOC - Signal Processing on Cloud</span
             >
           </a>
@@ -20,22 +20,22 @@ import logo from '../assets/img/logo.png';
             <div class="flex space-x-2">
               <a
                   href="index.html"
-                  class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  class="text-[#F2E74B] bg-[#735620] hover:bg-[#F2B441] hover:text-[#010D00] rounded-md px-3 py-2"
               >Home</a
               >
               <a
                   href="jobs.html"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  class="text-[#F2E74B] hover:bg-[#F2D544] hover:text-[#010D00] rounded-md px-3 py-2"
               >Visualizador</a
               >
               <a
                   href="add-job.html"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  class="text-[#F2E74B] hover:bg-[#F2D544] hover:text-[#010D00] rounded-md px-3 py-2"
               >Herramientas</a
               >
               <a
                   href="add-job.html"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  class="text-[#F2E74B] hover:bg-[#F2D544] hover:text-[#010D00] rounded-md px-3 py-2"
               >Documentación</a
               >
             </div>
@@ -47,5 +47,5 @@ import logo from '../assets/img/logo.png';
 </template>
 
 <style scoped>
-
+/* Aquí puedes agregar estilos adicionales si es necesario */
 </style>

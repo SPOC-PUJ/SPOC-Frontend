99ee63f1affa30b940330a33fa3a1c627b0e629d
Alberto Vigna <betoveluis@gmail.com>
2024-08-29 02:34:52 -0500

    Changed from "LoadFileCard" to "UploadFileButtonCard".

--- src/views/SignalVisualizerView.vue
+++ src/views/SignalVisualizerView.vue
@@ -6,7 +6,7 @@ import SumComponent from '@/components/SumComponent.vue';
 import MovingAverage from '@/components/MovingAverage.vue';
 const fileName = ref(null);
 
-import LoadFileCard from "@/components/LoadFileCard.vue";
+import UploadFileButtonCard from "@/components/UploadFileButtonCard.vue";
 
 const realValues = ref([]); // Array para almacenar los valores reales y pasarlos al graficador
 
@@ -19,7 +19,7 @@ function handleFileProcessed(values) {
 </script>
 
 <template>
-  <LoadFileCard @fileProcessed="handleFileProcessed"/>
+  <UploadFileButtonCard @fileProcessed="handleFileProcessed"/>
   <div class="border-dotted border-4 border-sky-500">
     <SimpleChart v-bind:data="realValues"/> <!-- Pasar los valores reales al graficador -->
     <!-- <SumComponent/> -->

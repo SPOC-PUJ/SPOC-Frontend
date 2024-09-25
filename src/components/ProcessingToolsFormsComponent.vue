c61c9f9855e2bc6366f2226e04406217357f5b7e
Alberto Vigna <betoveluis@gmail.com>
2024-09-25 17:32:01 -0500

    Fast Wavelet Transform Modularized.

--- src/components/ProcessingToolsFormsComponent.vue
+++ src/components/ProcessingToolsFormsComponent.vue
@@ -3,6 +3,7 @@ import {ref, watch} from 'vue';
 import {useSignalStore} from '@/stores/signalStore';
 import MovingAverage from "@/components/MovingAverage.vue";
 import FastWaveletHarr from "@/components/FastWaveletHarr.vue";
+import FastWaveletTransform from "@/components/FastWaveletTransform.vue";
 
 // Props
 const props = defineProps({
@@ -20,11 +21,7 @@ const selectedTool = ref('');
 
 // Datos para cada formulario
 
-// Tool 2: Fast Wavelet Transform
-const tool2Data = ref({
-  decLevel: '',
-  waveName: 'db1' // valor por defecto
-});
+
 
 // Tool 3: First Difference
 const tool3Data = ref({
@@ -53,14 +50,7 @@ watch(
 
 // Validación de datos y submit
 
-// Tool 2: Fast Wavelet Transform
-const submitTool2 = () => {
-  if (!tool2Data.value.decLevel) {
-    console.error('Error: Falta llenar el campo Dec Level en Tool 2');
-  } else {
-    console.log('Tool 2 Data:', tool2Data.value, 'Real Values:', props.data);
-  }
-};
+
 
 // Tool 3: First Difference
 const submitTool3 = () => {
@@ -121,26 +111,7 @@ const submitTool6 = () => {
 
     <!-- Formulario para la Herramienta 2: Fast Wavelet Transform -->
     <div v-if="selectedTool === 'FastWaveletTransform'" class="mb-6">
-      <h4 class="text-lg font-semibold text-green-500 mb-4">Fast Wavelet Transform</h4>
-      <form @submit.prevent="submitTool2">
-        <!-- Dec Level -->
-        <label class="block text-sm font-medium text-gray-700 mb-2">Dec Level:</label>
-        <input type="text" v-model="tool2Data.decLevel"
-               class="block w-full rounded-md border-gray-300 shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm mb-4"/>
-
-        <!-- Wave Name (Select Menu) -->
-        <label class="block text-sm font-medium text-gray-700 mb-2">Wave Name:</label>
-        <select v-model="tool2Data.waveName"
-                class="block w-full rounded-md border-gray-300 shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm mb-4">
-          <option value="db1">Daubechies 1 (db1)</option>
-          <option value="db2">Daubechies 2 (db2)</option>
-          <option value="coif1">Coiflet 1 (coif1)</option>
-          <option value="sym2">Symlet 2 (sym2)</option>
-        </select>
-
-        <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">Submit Tool 2
-        </button>
-      </form>
+      <FastWaveletTransform/>
     </div>
 
 

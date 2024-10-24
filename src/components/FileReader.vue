f9182e4abd238317f90cd9ce227dd614bcc855d3
Alberto Vigna <betoveluis@gmail.com>
2024-10-24 02:16:33 -0500

    Implemented danger modal to the file reader system.

--- src/components/FileReader.vue
+++ src/components/FileReader.vue
@@ -8,18 +8,28 @@ import { ABFReaderService } from "@/services/ReaderService.js";
 import { MATReaderService } from "@/services/ReaderMatService.js";
 import ProcessingToolsDangerModal from "@/components/dangerModals/ProcessingToolsDangerModal.vue";
 
-const showDangerModal = ref(false); // Controlar la visibilidad del modal.
-const incompatibleFileName = ref(''); // Almacenar el nombre del archivo incompatible.
-const incompatibleFileExtension = ref(''); // Almacenar la extensión del archivo incompatible.
-const fileInputRef = ref(null); // Referencia al input de archivo.
+const showDangerModal = ref(false); // Controlar la visibilidad del modal para archivos incompatibles
+const incompatibleFileName = ref(''); // Almacenar el nombre del archivo incompatible
+const incompatibleFileExtension = ref(''); // Almacenar la extensión del archivo incompatible
+const fileInputRef = ref(null); // Referencia al input de archivo
 
 const signalStore = useSignalStore();
-const emit = defineEmits(['fileProcessed']); // Emitir eventos personalizados.
+const emit = defineEmits(['fileProcessed']); // Emitir eventos personalizados
 
 const output = ref('');
 
 // Estado de carga reactivo
 const loadingStatus = ref(false);
+
+// Variables para el ProcessingToolsDangerModal
+const showModal = ref(false);
+const modalTitle = ref('Error al procesar el archivo');
+const modalMessage = ref('');
+
+// Función para manejar la confirmación del modal
+const handleModalConfirm = () => {
+  showModal.value = false;
+};
 
 const processFile = (event) => {
   const file = event.target.files[0];
@@ -35,83 +45,98 @@ const processFile = (event) => {
     loadingStatus.value = true;
 
     reader.onload = async () => {
-      const data = new Uint8Array(reader.result);
+      try {
+        const data = new Uint8Array(reader.result);
 
-      // Crear un archivo en el sistema de archivos del módulo WebAssembly
-      Module.FS_createDataFile('/', 'filename', data, true, true, true);
+        // Crear un archivo en el sistema de archivos del módulo WebAssembly
+        Module.FS_createDataFile('/', 'filename', data, true, true, true);
 
-      // Crear una instancia del lector EDF
-      const edfInstance = new Module.EDF('filename');
+        // Crear una instancia del lector EDF
+        const edfInstance = new Module.EDF('filename');
 
-      // Uso de la instancia EDF
-      edfInstance.PrintHeaderRecords();
-      edfInstance.PrintDataRecords();
-      edfInstance.PrintSizeSignals();
-      edfInstance.PrintTopValues(5);
+        // Uso de la instancia EDF
+        edfInstance.PrintHeaderRecords();
+        edfInstance.PrintDataRecords();
+        edfInstance.PrintSizeSignals();
+        edfInstance.PrintTopValues(5);
 
-      console.log(edfInstance);
+        console.log(edfInstance);
 
-      // Obtener la instancia de señales
-      const signalsInstance = edfInstance.Signals;
+        // Obtener la instancia de señales
+        const signalsInstance = edfInstance.Signals;
 
-      console.log(signalsInstance);
+        console.log(signalsInstance);
 
-      signalsInstance.CalculateMeans();
-      signalsInstance.CalculateDeviation();
-      signalsInstance.PrintMeanAndDeviation();
+        signalsInstance.CalculateMeans();
+        signalsInstance.CalculateDeviation();
+        signalsInstance.PrintMeanAndDeviation();
 
-      var sigInstanc = signalsInstance.signals;
-      signalStore.setSignalObject(sigInstanc);
+        var sigInstanc = signalsInstance.signals;
+        signalStore.setSignalObject(sigInstanc);
 
-      console.log(sigInstanc);
-      if (sigInstanc.size() !== 0) {
-        console.log(sigInstanc.size());
-        console.log('Señales encontradas.');
-      }
-
-      // Almacenar los valores reales en un array y emitirlos
-      var veceigen = sigInstanc.get(0);
-      const realValues = []; // Array para almacenar los valores reales
-      for (let i = 0; i < veceigen.size; i++) {
-        var complexValue = veceigen.get(i);
-        realValues.push(complexValue.real()); // Almacenar la parte real
-      }
-
-      // Emitir los valores reales procesados
-      emit('fileProcessed', realValues); // Emitir los valores para el graficador
-
-      // Crear el mensaje para gRPC
-      const signalData = [];
-      for (let i = 0; i < sigInstanc.size(); i++) {
-        const vectorEigen = sigInstanc.get(i);
-        const complexArray = [];
-        for (let j = 0; j < vectorEigen.size; j++) {
-          var complexValue = vectorEigen.get(j);
-          complexArray.push(Complex.create({real: complexValue.real(), imag: complexValue.imag()}));
+        console.log(sigInstanc);
+        if (sigInstanc.size() !== 0) {
+          console.log(sigInstanc.size());
+          console.log('Señales encontradas.');
         }
-        signalData.push({values: complexArray}); // Añadir el array de complejos
+
+        // Almacenar los valores reales en un array y emitirlos
+        var veceigen = sigInstanc.get(0);
+        const realValues = []; // Array para almacenar los valores reales
+        for (let i = 0; i < veceigen.size; i++) {
+          var complexValue = veceigen.get(i);
+          realValues.push(complexValue.real()); // Almacenar la parte real
+        }
+
+        // Emitir los valores reales procesados
+        emit('fileProcessed', realValues); // Emitir los valores para el graficador
+
+        // Crear el mensaje para gRPC
+        const signalData = [];
+        for (let i = 0; i < sigInstanc.size(); i++) {
+          const vectorEigen = sigInstanc.get(i);
+          const complexArray = [];
+          for (let j = 0; j < vectorEigen.size(); j++) {
+            var complexValue = vectorEigen.get(j);
+            complexArray.push(Complex.create({real: complexValue.real(), imag: complexValue.imag()}));
+          }
+          signalData.push({values: complexArray}); // Añadir el array de complejos
+        }
+        signalStore.setSignalJson(signalData);
+        console.log("En el signalStore (edf):", toRaw(signalStore.signalJson));
+
+        output.value = 'Archivo .edf procesado exitosamente';
+
+        // Desactivar el estado de carga
+        loadingStatus.value = false;
+
+        event.target.value = ''; // Limpiar el input de archivo
+
+        // Liberar memoria de los objetos de WebAssembly
+        edfInstance.delete();
+        signalsInstance.delete();
+        Module.FS_unlink('/filename');
+      } catch (error) {
+        console.error('Error al procesar el archivo .edf:', error);
+        modal
+        modalMessage.value = 'Error al procesar el archivo .edf: ' + error.message;
+        showModal.value = true;
+        loadingStatus.value = false;
+        event.target.value = ''; // Limpiar el input de archivo
       }
-      signalStore.setSignalJson(signalData);
-      console.log("En el signalStore (edf):", toRaw(signalStore.signalJson));
+    };
 
-      output.value = 'Archivo .edf procesado exitosamente';
-
-      // Desactivar el estado de carga
+    reader.onerror = (error) => {
+      console.error("Error al leer el archivo .edf:", error);
+      modalMessage.value = 'Error al leer el archivo .edf: ' + error.message;
+      showModal.value = true;
       loadingStatus.value = false;
-
       event.target.value = ''; // Limpiar el input de archivo
-
-      // Liberar memoria de los objetos de WebAssembly
-      edfInstance.delete();
-      signalsInstance.delete();
-      Module.FS_unlink('/filename');
     };
 
     reader.readAsArrayBuffer(file); // Leer como datos binarios
   } else if (file.name.toLowerCase().endsWith(".abf")) {
     output.value = "Formato Correcto (.abf)";
-
-    // . abf_env/bin/activate  --> python ABFReaderServer.py
 
     // Activar el estado de carga
     loadingStatus.value = true;
@@ -137,8 +162,13 @@ const processFile = (event) => {
         console.log("En el signalStore (abf):", toRaw(signalStore.signalJson));
 
         output.value = 'Archivo .abf procesado exitosamente';
+
+        // Emitir los valores reales procesados
+        emit('fileProcessed', rawNumbers); // Emitir los valores para el graficador
       } catch (error) {
-        console.error('Error al subir el archivo:', error);
+        console.error('Error al subir el archivo .abf:', error);
+        modalMessage.value = 'Error al subir el archivo .abf: ' + error.message;
+        showModal.value = true;
       } finally {
         // Desactivar el estado de carga
         loadingStatus.value = false;
@@ -148,7 +178,11 @@ const processFile = (event) => {
     };
 
     reader.onerror = (error) => {
-      console.error("Error al leer el archivo:", error);
+      console.error("Error al leer el archivo .abf:", error);
+      modalMessage.value = 'Error al leer el archivo .abf: ' + error.message;
+      showModal.value = true;
+      loadingStatus.value = false;
+      event.target.value = ''; // Limpiar el input de archivo
     };
 
     reader.readAsArrayBuffer(file); // Leer el archivo como ArrayBuffer
@@ -183,7 +217,9 @@ const processFile = (event) => {
         // Emitir los valores reales procesados
         emit('fileProcessed', rawNumbers); // Emitir los valores para el graficador
       } catch (error) {
-        console.error('Error al subir el archivo:', error);
+        console.error('Error al subir el archivo .mat:', error);
+        modalMessage.value = 'Error al subir el archivo .mat: ' + error.message;
+        showModal.value = true;
       } finally {
         // Desactivar el estado de carga
         loadingStatus.value = false;
@@ -193,7 +229,11 @@ const processFile = (event) => {
     };
 
     reader.onerror = (error) => {
-      console.error("Error al leer el archivo:", error);
+      console.error("Error al leer el archivo .mat:", error);
+      modalMessage.value = 'Error al leer el archivo .mat: ' + error.message;
+      showModal.value = true;
+      loadingStatus.value = false;
+      event.target.value = ''; // Limpiar el input de archivo
     };
 
     reader.readAsArrayBuffer(file); // Leer el archivo como ArrayBuffer
@@ -208,13 +248,13 @@ const processFile = (event) => {
   }
 };
 
-// Función para transformar los números en objetos de señales con duplas (para .abf y .mat).
+// Función para transformar los números en objetos de señales con duplas (para .abf y .mat)
 function transformToSignalObjectWithReal(numbers) {
   const values = numbers.map(num => ({
     real: num, // Mapeamos cada número a un objeto con la propiedad 'real'
   }));
 
-  return {values}; // Devolvemos un objeto con los valores como propiedad
+  return { values }; // Devolvemos un objeto con los valores como propiedad
 }
 
 const handleRetry = () => {
@@ -234,31 +274,41 @@ onMounted(() => {
 
 <template>
   <div>
+    <!-- Modal para mostrar errores durante el procesamiento -->
     <ProcessingToolsDangerModal
         v-if="showModal"
+        :title="modalTitle"
         :message="modalMessage"
         @close="showModal = false"
         @confirm="handleModalConfirm"
     />
 
-    <DangerModal v-if="showDangerModal" @close="showDangerModal = false" @retry="handleRetry"
-                 :fileName="incompatibleFileName" :fileExtension="incompatibleFileExtension"/>
-    <!-- Mostrar el modal de error -->
+    <!-- Modal para archivos incompatibles -->
+    <DangerModal
+        v-if="showDangerModal"
+        @close="showDangerModal = false"
+        @retry="handleRetry"
+        :fileName="incompatibleFileName"
+        :fileExtension="incompatibleFileExtension"
+    />
+
+    <!-- Botón para cargar archivo -->
     <div class="flex justify-center items-center">
       <label for="fileInputRef"
-             class="pi pi-upload cursor-pointer inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Cargar
-        Archivo</label>
+             class="pi pi-upload cursor-pointer inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Cargar Archivo
+      </label>
     </div>
 
-    <div class="flex justify-center items-center w-full h-full fixed inset-0 m-auto bg-white bg-opacity-70"
-         v-if="loadingStatus">
+    <!-- Loader mientras se procesa el archivo -->
+    <div class="flex justify-center items-center w-full h-full fixed inset-0 m-auto bg-white bg-opacity-70" v-if="loadingStatus">
       <div class="transform scale-[2] flex flex-col justify-center items-center">
-        <JellyfishLoader color="#3B82F6"/>
+        <JellyfishLoader color="#3B82F6" />
         <h2 class="text-blue-500 mt-4">Procesando archivo...</h2>
       </div>
     </div>
 
-    <input id="fileInputRef" type="file" @change="processFile" accept=".edf,.abf,.mat" hidden/>
+    <input id="fileInputRef" type="file" @change="processFile" accept=".edf,.abf,.mat" hidden />
     <pre hidden>{{ output }}</pre> <!-- Mensaje de salida oculto -->
   </div>
 </template>

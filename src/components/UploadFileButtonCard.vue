6c0ad173ee78dd9ec0418747543cd26e5d7c3cbc
Alberto Vigna <betoveluis@gmail.com>
2024-10-24 01:05:15 -0500

    Renamed FileReader.

--- src/components/UploadFileButtonCard.vue
+++ src/components/UploadFileButtonCard.vue
@@ -1,16 +1,16 @@
 <script setup>
-import EDFReader from '@/components/EdfReader.vue';
+import FileReader from "@/components/FileReader.vue";
 </script>
 
 <template>
   <div class="bg-white rounded-xl shadow-md relative">
       <div class="mb-6 p-4 border-red-400 border-2">
         <div class="absolute inset-y-0 right-0 p-4">
-          <i class="pi pi-exclamation-triangle text-orange-700 ">Extensiones de archivos admitidos: .edf y .abf</i>
+          <i class="pi pi-exclamation-triangle text-orange-700 ">Extensiones de archivos admitidos: .edf, .abf, .mat</i>
         </div>
         <div class="p-4">
           <!-- Botón de carga de archivos delegado a EDFReader -->
-          <EDFReader @fileProcessed="$emit('fileProcessed', $event)"/>
+          <FileReader @fileProcessed="$emit('fileProcessed', $event)"/>
         </div>
       </div>
   </div>

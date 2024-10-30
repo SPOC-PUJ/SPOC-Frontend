14a369d9e10a46ddaf224d6b59891633f886efc7
Alberto Vigna <betoveluis@gmail.com>
2024-10-30 16:25:04 -0500

    Improved ResetZoom button.

--- src/components/SimpleChart.vue
+++ src/components/SimpleChart.vue
@@ -559,7 +559,8 @@ onMounted(() => {
   const resetZoomButton = d3
       .select(chartContainer.value)
       .append('button')
-      .text('Restablecer Zoom')
+      .html(`<i class="pi pi-search-minus mr-2"></i>Restablecer Zoom`)
+      .attr('class', 'flex items-center px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75')
       .style('position', 'absolute')
       .style('top', '10px')
       .style('right', '10px')
